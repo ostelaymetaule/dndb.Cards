4bfcd6b6c832f87d6f8ed706b88e6179ed77e407
Alexander Elgin <alexander.elgin@hotmail.com>
2020-01-27 21:52:54 +0100

    Initiate telegram bot project

---
+++ dndb.Cards.Bot/CardParserBot.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace dndb.Cards.Bot
+{
+    public class CardParserBot
+    {
+        private ITelegramBotClient _botClient;
+
+
+        public CardParserBot(string token)
+        {
+            _botClient = new TelegramBotClient(token);
+
+            var me = _botClient.GetMeAsync().Result;
+            Console.WriteLine(
+              $"Hello, World! I am user {me.Id} and my name is {me.FirstName}."
+            );
+
+            _botClient.OnMessage += Bot_OnMessage;
+            _botClient.StartReceiving();
+            Thread.Sleep(int.MaxValue);
+
+
+        }
+
+        private async void Bot_OnMessage(object sender, MessageEventArgs e)
+        {
+            if (e.Message.Text != null)
+            {
+                Console.WriteLine($"Received a text message in chat {e.Message.Chat.Id}.");
+
+                await _botClient.SendTextMessageAsync(
+                  chatId: e.Message.Chat,
+                  text: "You said:\n" + e.Message.Text
+                );
+            }
+        }
+    }
+}
---
+++ dndb.Cards.Bot/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace dndb.Cards.Bot
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("starting bot!");
+            string token = "1004803078:AAF-GFggKCPeVxIyUS94L3mbP3xohkPxiFU";
+            var myBotListner = new CardParserBot(token);
+
+
+           
+            Console.WriteLine("ending bot");
+
+        }
+
+       
+    }
+}
